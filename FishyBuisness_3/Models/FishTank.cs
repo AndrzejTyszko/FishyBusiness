f2f948cada0295e7356b52e48d998ee866705b4b
KacperKabzinski <kackab056@gmail.com>
2024-01-08 23:23:58 +0100

    changes

---
+++ FishyBuisness_3/Models/FishTank.cs
@@ -0,0 +1,13 @@
+﻿namespace FishyBuisness_3.Models
+{
+    public class FishTank
+    {
+        public int FishTankId { get; set; }
+        public string Name { get; set; }
+        public string Descirption { get; set; }
+        public int Capacity { get; set; }
+        public int EnvironmentId {  get; set; }
+
+        public Environment Environment{ get; set; }
+    }
+}
