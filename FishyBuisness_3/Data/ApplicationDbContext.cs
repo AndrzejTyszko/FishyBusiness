be21ad2fc48f7acbb43c4ea5f1902bd4437a4a94
KacperKabzinski <kackab056@gmail.com>
2024-01-08 23:23:58 +0100

    changes

---
+++ FishyBuisness_3/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using FishyBuisness_3.Models;
+
+namespace FishyBuisness_3.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+	    public DbSet<FishyBuisness_3.Models.Fish> Fish { get; set; } = default!;
+        public DbSet<FishyBuisness_3.Models.ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Warehouse> Warehouses { get; set; }
+        public DbSet<WarehouseItem> WarehouseItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Konfiguracja relacji między WarehouseItem, Product i Warehouse
+            modelBuilder.Entity<WarehouseItem>()
+                .HasOne(wi => wi.Product)
+                .WithMany()
+                .HasForeignKey(wi => wi.ProductId);
+
+            modelBuilder.Entity<WarehouseItem>()
+                .HasOne(wi => wi.Warehouse)
+                .WithMany()
+                .HasForeignKey(wi => wi.WarehouseId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ FishyBuisness_3/Models/Fish.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FishyBuisness_3.Models
+{
+	public class Fish
+	{
+		public int FishId { get; set; }
+
+		[Required]
+		[MaxLength(60, ErrorMessage = "Max Length length should not be more than 30 characters" )]
+		[MinLength(3, ErrorMessage = "Product name should be at least 3 characters")]
+		public string FishName { get; set; }
+
+        [MaxLength(60, ErrorMessage = "Max Length length should not be more than 30 characters")]
+        [MinLength(3, ErrorMessage = "Product name should be at least 3 characters")]
+        public string FishDescription { get; set; }
+		public string Spieces { get; set; }
+		public string Habitat { get; set; }
+
+        [Required]
+        [Range(0, 1000, ErrorMessage = "Can not be less than 0.1 and more than 100")]
+        public double Lenght { get; set; }
+
+        [Required]
+        [Range(0, 1000, ErrorMessage = "Can not be less than 0.1 and more than 1000")]
+        public double Weight { get; set; }
+
+		[Required]
+		[Range(0, 100000, ErrorMessage = "Can not be less than 0.1 and more than 100000")]
+		public double Price { get; set; }
+
+	}
+}
