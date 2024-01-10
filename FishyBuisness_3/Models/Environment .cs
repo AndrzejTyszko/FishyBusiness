e39bc1bae51103276e60a9e72470ece0dbf50eca
KacperKabzinski <kackab056@gmail.com>
2024-01-10 14:09:15 +0100

    Changes2

---
+++ FishyBuisness_3/Models/Environment .cs
@@ -0,0 +1,9 @@
+﻿namespace FishyBuisness_3.Models
+{
+    public class Environment
+    {
+        public int EnvironmentId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ FishyBuisness_3/Models/Stock.cs
@@ -0,0 +1,21 @@
+﻿namespace FishyBuisness_3.Models
+{
+    public class Stock
+    {
+        public int StockId { get; set; }
+        public int FishId { get; set; }
+        public int FishTankId { get; set; }
+        public int Quantity { get; set; }
+        public int EnvironmentId {  get; set; }
+
+        // Klucz obcy do modelu Product
+        public Fish? Fish { get; set; }
+
+        // Klucz obcy do modelu Warehouse
+        public FishTank? FishTank { get; set; }
+
+        public Environment? Environment { get; set; }
+
+    }
+
+}
