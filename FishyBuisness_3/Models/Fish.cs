bf13b01b39ceab0f5e6da9b3d4657fe96a3c0d29
Andrzej Tyszko <a.tyszko@medicalgorithmics.com>
2024-01-12 11:57:13 +0100

    Tlumaczenie ryby

--- FishyBuisness_3/Models/Fish.cs
+++ FishyBuisness_3/Models/Fish.cs
@@ -10,12 +10,12 @@ namespace FishyBuisness_3.Models
 
 		[Required]
         [DisplayName("Fish name")]
-		//[MaxLength(60, ErrorMessage = "Max Length length should not be more than 30 characters" )]
-		//[MinLength(3, ErrorMessage = "Product name should be at least 3 characters")]
+		[MaxLength(60, ErrorMessage = "Max Length length should not be more than 60 characters" )]
+		[MinLength(3, ErrorMessage = "Product name should be at least 3 characters")]
 		public string FishName { get; set; }
 
-        //  [MaxLength(60, ErrorMessage = "Max Length length should not be more than 30 characters")]
-        //  [MinLength(3, ErrorMessage = "Product name should be at least 3 characters")]
+        [MaxLength(60, ErrorMessage = "Max Length length should not be more than 30 characters")]
+        [MinLength(3, ErrorMessage = "Product name should be at least 3 characters")]
         [DisplayName("Fish Description")]
         public string FishDescription { get; set; }
 
@@ -26,18 +26,18 @@ namespace FishyBuisness_3.Models
         public string Habitat { get; set; }
         [DisplayName("Envirnment ID")]
         public int EnvironmentId { get; set; }
-        //  [Required]
-        //   [Range(0, 1000, ErrorMessage = "Can not be less than 0.1 and more than 100")]
+        [Required]
+        [Range(0, 1000, ErrorMessage = "Can not be less than 0.1 and more than 100")]
         [DisplayName("Length")]
         public double Lenght { get; set; }
 
-        //  [Required]
-        //  [Range(0, 1000, ErrorMessage = "Can not be less than 0.1 and more than 1000")]
+        [Required]
+        [Range(0, 1000, ErrorMessage = "Can not be less than 0.1 and more than 1000")]
         [DisplayName("Weigth")]
         public double Weight { get; set; }
 
-        //	[Required]
-        //	[Range(0, 100000, ErrorMessage = "Can not be less than 0.1 and more than 100000")]
+        [Required]
+        [Range(0, 100000, ErrorMessage = "Can not be less than 0.1 and more than 100000")]
         [DisplayName("Price")]
       
         public double Price { get; set; }
